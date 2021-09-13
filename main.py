45d8418eb6586137b1a460990958b819938faa4c
Madison-Skogsberg-2023 <90402622+Madison-Skogsberg-2023@users.noreply.github.com>
2021-09-13 09:48:54 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+#print title
+print("My top 5 movies:")
+#print 1st movie
+print("The Spongebob Movie (the original one, not the bad one)")
+#print 2nd movie
+print("The Phineas and Ferb series")
+#print 3rd movie
+print("Psych series")
+#print 4th movie
+print("Numb3rs series")
+#print 5th movie
+print("The Twilight Zone series")
