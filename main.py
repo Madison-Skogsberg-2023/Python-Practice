6651e1a8ca4528bd24c1389f329fe52f628eb706
Madison-Skogsberg-2023 <816418@mystma.org>
2021-09-14 09:35:13 -0500

    Added movies

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-#print title
-print("My top 5 movies:")
-#print 1st movie
-print("The Spongebob Movie (the original one, not the bad one)")
-#print 2nd movie
-print("The Phineas and Ferb series")
-#print 3rd movie
-print("Psych series")
-#print 4th movie
-print("Numb3rs series")
-#print 5th movie
-print("The Twilight Zone series")
+def topFiveMovies():
+    #print title
+    print("My Top Five Movies:")
+
+    #print 1st movie
+    print("1. The Phineas and Ferb Series")
+
+    #print 2nd movie
+    print("2. The Spongebob Squarepants Series")
+
+    #print 3rd movie
+    print("3. The Original Spongebob Squarepants Movie")
+
+    #print 4th movie
+    print("4. The Psych Series")
+
+    #print 5th movie
+    print("5. The Numb3rs Series")
+
+#run topFiveMovies
+topFiveMovies()
