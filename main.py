d95527ff0ab03804cc4e0ee258b17c723b6708e3
Madison-Skogsberg-2023 <816418@mystma.org>
2021-09-15 09:15:16 -0500

    Created Functions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+#define function
 def topFiveMovies():
     #print title
     print("My Top Five Movies:")
@@ -18,19 +18,84 @@ def topFiveMovies():
     #print 5th movie
     print("5. The Numb3rs Series")
 
-#run topFiveMovies
-topFiveMovies()
-=======
-#print title
-print("My top 5 movies:")
-#print 1st movie
-print("The Spongebob Movie (the original one, not the bad one)")
-#print 2nd movie
-print("The Phineas and Ferb series")
-#print 3rd movie
-print("Psych series")
-#print 4th movie
-print("Numb3rs series")
-#print 5th movie
-print("The Twilight Zone series")
->>>>>>> 45d8418eb6586137b1a460990958b819938faa4c
+#define function
+def favoriteFoods():
+    #print title
+    print("These are my favorite foods:")
+
+    #print 1st food
+    print("1. Cheesy Garlic Bread")
+
+    #print 2nd food
+    print("2. Pizza")
+
+    #print 3rd food
+    print("3. Rice")
+
+    #print 4th food
+    print ("4. Pasta, Noodles, Lo Mein, you get the idea")
+
+    #print 5th food
+    print("5. Doughnuts")
+
+#define function
+def morningRoutine():
+    #print title
+    print("This is my morning routine:")
+
+    #print 1st task
+    print("1. Try to find the will to get out of bed")
+
+    #print 2nd task
+    print("2. Don't get out of bed")
+
+    #print 3rd task
+    print("3. Panic because you're going to be late for school")
+
+    #print 4th task
+    print("4. Getdressedgetcoffeegetbackpackhurryhurryhurry")
+
+    #print 5th task
+    print("5. Wait 20 minutes for the bus that was supposed to show up 20 minutes ago but it's always late even though you rushed out of the door this morning to catch the bus you think you'd've learned to lower your expectations by now")
+
+#define function
+def dreamJob():
+    #print title
+    print("These are some of my dream jobs:")
+
+    #print job 1
+    print("1. Quantum Mathematician at CERN")
+
+    #print job 2
+    print("2. Astronaut")
+
+    #print job 3
+    print("3. Mathematics Professor")
+
+#define function
+def bohemianRhapsody():
+    #print title
+    print("You just got Bohemian Rhapsody'd")
+
+    #print song lyrics
+    print("Is this the real life/ Is this just fantasy/ Caught in a landslide/ No escape from reality Etc...")
+
+#define function
+def myCat():
+    #print title
+    print ("Why I love my cat even though she's a jerk:")
+
+    #print reason 1
+    print("1. Fluffy")
+
+    #print reason 2
+    print("2. Sometimes she licks my hand")
+
+    #print reason 3
+    print("3. She is very small")
+
+    #print reason 4
+    print("4. The little 'brrp' noise she makes when she wakes up")
+
+    #print reason 5
+    print ("5. She looks like a loaf of bread when she sits down")
