b6dd4bc89a679c3aab8064b2b622f4ab24a02507
Madison-Skogsberg-2023 <816418@mystma.org>
2021-09-16 09:42:22 -0500

    Mad Libs Created

--- function.py
+++ function.py
@@ -1,7 +1,12 @@
 #define function
-def timesTwo(x):
-    
-    #output string
-    print(x *2)
+def madLib(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w):
 
-timesTwo(30)
+    #output story
+    print("Star Wars is a " + a + b + " of " + c + " versus evil in a " + d + " far far away.")
+    print("There are " + e + " battles between " + f + g + " in " + h + " space and " + i + " duels with " + j + " called " + k + "-sabers. ")
+    print(l + " called 'droids' are helpers and " + m + " to the heroes." )
+    print("A " + n + "power called the " + o + p + "s people to do " + q + " things, like " + r + s + ". ")
+    print("The Jedi " + t + "use The Force for the " + u + "side and the Sith " + v + "it for the " + w + "side.")
+
+#run function
+madLib("silly", "chicken", "purple", "McDonald's", "interesting", "tired", "school buses", "tasty", "intense", "tricycles", "uber", "Offices", "chairs",)
