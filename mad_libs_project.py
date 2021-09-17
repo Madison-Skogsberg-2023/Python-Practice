9840c36c66445d61f955839ed8fec77f724839ca
Madison-Skogsberg-2023 <816418@mystma.org>
2021-09-17 09:13:53 -0500

    comments

--- mad_libs_project.py
+++ mad_libs_project.py
@@ -1,7 +1,8 @@
 #define function
 def madLib(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w):
 
-    #output story
+    #print story
+    #insert variables
     print("Star Wars is a " + a + b + " of " + c + " versus evil in a " + d + " far far away.")
     print("There are " + e + " battles between " + f + g + " in " + h + " space and " + i + " duels with " + j + " called " + k + "-sabers. ")
     print(l + " called 'droids' are helpers and " + m + " to the heroes." )
@@ -9,4 +10,5 @@ def madLib(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w):
     print("The Jedi " + t + "use The Force for the " + u + "side and the Sith " + v + "it for the " + w + "side.")
 
 #run function
+#define variables
 madLib("live-action Netflix remake of a ", "story", "clueless farmers", "big frickin metal ball", "lazy", "tired ", "old men", "a depressing", "repetitive", "dangerous sticks", "uber", "Robots", "essentially fan-service", "vague and poorly-written ", "Force ", "help", "irrelevent", "reuniting with their illegitamite ", "children", "sometimes ", "uwu ", "use ", "meanie ")
