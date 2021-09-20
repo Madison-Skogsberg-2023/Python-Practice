a57980789f794cb952347270393ce7e83af40eb1
Madison-Skogsberg-2023 <816418@mystma.org>
2021-09-20 09:14:07 -0500

    return functions, rename files, created main

---
+++ main.py
@@ -0,0 +1,17 @@
+#define function
+def cat():
+    print("Cat")
+    return "Cat"
+
+#define function
+def dog():
+    return "dog"
+
+#print return
+print(cat() + dog())
+
+#define function
+def planets():
+    return 8
+
+print(planets())
---
+++ tasks.py
@@ -0,0 +1,6 @@
+#define function
+def woof():
+    return "woof"
+
+#run function
+print(woof())
